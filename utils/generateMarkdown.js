03977a660f3f042db6bd05c3fde602c0148b591b
TennCreekBridges <bridgettvalet@gmail.com>
2022-02-14 16:12:40 -0600

    renders installation and license to readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// create a function that returns a license badge based on which license is passed in and if there is no license, return an empty string
+// creates a function that returns a license badge based on which license is passed in and if there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "None") {
     return 'No License';
@@ -7,7 +7,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// create a function that returns the license link, and if there is no license, return an empty string
+// creates a function that returns the license link, and if there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "None") {
     return "";
@@ -16,11 +16,26 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// creates a function that returns the license section of README and if there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return ["", ""];
+  } else {
+    return [`## License \nThis project was developed under the ${license} license.`, `* [License](#License)`];
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// generate installation instructions or blank string if not available
+function renderInstallationSection(install) {
+  if (!install) {
+    return ["", ""];
+  } else { 
+    return [`## Installation \n${install}`, `* [Installation](#Installation)`];
+  }
+}
+
+
+// creates a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
