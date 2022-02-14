7c37a5c722c2477f17e1270deb59135f19e91edd
TennCreekBridges <bridgettvalet@gmail.com>
2022-02-14 16:29:17 -0600

    creates function to generate markdown for readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // creates a function that returns a license badge based on which license is passed in and if there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "None") {
-    return 'No License';
+    return "No License";
   } else {
     return `![License](https://img.shields.io/badge/License-${license}-brightgreen)`;
   }
@@ -12,7 +12,7 @@ function renderLicenseLink(license) {
   if (license === "None") {
     return "";
   } else {
-    return `[More information about ${license}](https://opensource.org/licenses/${license})`
+    return `[More information about ${license}](https://opensource.org/licenses/${license})`;
   }
 }
 
@@ -21,7 +21,10 @@ function renderLicenseSection(license) {
   if (license === "None") {
     return ["", ""];
   } else {
-    return [`## License \nThis project was developed under the ${license} license.`, `* [License](#License)`];
+    return [
+      `## License \nThis project was developed under the ${license} license.`,
+      `* [License](#License)`,
+    ];
   }
 }
 
@@ -29,17 +32,46 @@ function renderLicenseSection(license) {
 function renderInstallationSection(install) {
   if (!install) {
     return ["", ""];
-  } else { 
+  } else {
     return [`## Installation \n${install}`, `* [Installation](#Installation)`];
   }
 }
 
-
 // creates a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+  
+${renderLicenseBadge(data.license)}
 
-`;
+## Description
+${data.description}
+
+## Table of Contents
+${renderInstallSection(data.installinstructions)[1]}
+
+* [Usage](#Usage)
+${renderLicenseSection(data.license)[1]}
+
+* [Contributions](#Contributions)
+
+* [Questions](#Questions)
+
+## Usage
+${data.usage}
+${renderLicenseSection(data.license)[0]}
+${renderLicenseLink(data.license)}
+
+## Contributions
+${data.contributions}
+
+## Questions
+You may direct any questions about the project to [${
+    data.github
+  }](https://github.com/${data.github}), via email: [${data.email}](mailto:${
+    data.email
+  }).
+  `;
 }
 
 module.exports = generateMarkdown;
